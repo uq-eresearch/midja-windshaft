f8aa4c8b3319e3f6dcb48cff53b37925beea9f37
Tim Dettrick <t.dettrick@uq.edu.au>
2017-04-26 18:06:12 +1000

    Change to use server.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const windshaft = require('windshaft')
+const server = require('./src/server')
 const fs = require('fs-promise') // Note: not built-in Node
 const args = process.argv.slice(2)
 
 async function run(configFileName, port) {
   const dbConfig = await fs.readJson(configFileName)
-  const db = pgp(dbConfig)
-
-  app.get('*', (req, res) => {
-    const queryStr = req.query.q
-
-    db.any(queryStr)
-      .then((result) => {
-        res.json({ rows: result })
-      })
-      .catch((ex) => {
-        res.status(500).send(ex.message)
-      })
-  })
-
-  app.listen(port)
-  console.log("midja-sql listening on "+port)
+  server(config).listen(port)
+  console.log("midja-windshaft listening on "+port)
 }
 
 run(args[0], args[1] || 3000)
